66c1a89ccbcdb34c14567f1cf249158a26f15bab
Gevork Seropyan <seropyangevork@rocketmail.com>
2023-06-21 20:11:15 -0400

    added post to notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -5,3 +5,31 @@ const {
     readAndAppend,
     writeToFile,
   } = require('../helpers/fsUtils');
+
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  });
+
+// POST Route for submitting a new note
+notes.post('/', (req, res) => {
+  // Destructuring syntax to pull out specific properties from req.body object
+  const { title, text } = req.body;
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuidv4(),
+    };
+    readAndAppend(newNote, './db/db.json');
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+    res.json(response);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
+module.exports = notes;
